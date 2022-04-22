9d94fcd680277809e2c85962c8e0f531ad74b79f
Aadish Jain <aadishjain2911@gmail.com>
2022-04-22 11:59:25 +0530

    signup page added

--- swiftbuy_backend/swiftbuy_backend/urls.py
+++ swiftbuy_backend/swiftbuy_backend/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.homepage, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
