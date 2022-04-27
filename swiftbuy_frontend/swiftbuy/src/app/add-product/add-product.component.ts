2f055ecec0af2c33235ed5940530bddfe04cdb0f
Purvi Poonia <waytopurvi2001@gmail.com>
2022-04-27 14:29:48 +0530

    create add product form - just frontend

--- swiftbuy_frontend/swiftbuy/src/app/add-product/add-product.component.ts
+++ swiftbuy_frontend/swiftbuy/src/app/add-product/add-product.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.scss']
 })
-export class AddProductComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
-
+export class AddProductComponent {
+  
 }
