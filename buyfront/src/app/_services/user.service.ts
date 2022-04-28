0f6fb3233a4c40c390bf3ec7ba650391db7c430e
Gaurang Dev <gaurangdev@Gaurangs-MacBook-Air.local>
2022-04-28 16:33:10 +0530

    login updated

--- buyfront/src/app/_services/user.service.ts
+++ buyfront/src/app/_services/user.service.ts
@@ -12,35 +12,32 @@ export class UserService {
     getNotifications() {
         console.log('getNotifications');
         // return this.http.get('http://localhost:8080/api/users/' + userid + '/notifications');
-        return this.http.get<Notification[]>(`http://localhost:8000/notifications`);
+        return this.http.get<Notification[]>(`http://192.168.0.104:8000/notifications`);
     }
 
     getcatalog() {
         console.log('getcatalog');
-        return this.http.get<Product[]>(`http://localhost:8000/home/catalog`);
+        return this.http.get<Product[]>(`http://192.168.0.104:8000/home/catalog`);
     }
 
     getProduct(productid: number) {
         console.log('getProduct productid: ' + productid);
-        return this.http.get<Product>(`http://localhost:8000/home/catalog/products/${productid}`);
+        return this.http.get<Product>(`http://192.168.0.104:8000/home/catalog/products/${productid}`);
 
     }
 
     getCart() {
         console.log('getCart');
-        return this.http.get<Product[]>('http://localhost:8000/home/cart');
+        return this.http.get<Product[]>('http://192.168.0.104:8000/home/cart');
     }
 
     getPlaceOrder(paymentid: number) {
-        return this.http.post(`http://localhost:8000/home/order`, (paymentid));
+        return this.http.post(`http://192.168.0.104:8000/home/order`, (paymentid));
     }
 
     register(user: User) {
         console.log('register user: ' + JSON.stringify(user));
-        return this.http.post('http://localhost:8000/signup/', (user)).subscribe(response => {
-            console.log(response);
-        });
-;
+        return this.http.post('http://192.168.0.104:8000/signup/', (user));
     }
 
     // delete(id: number) {
