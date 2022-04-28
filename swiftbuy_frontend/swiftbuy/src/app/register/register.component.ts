02e7703d47570bd785bb173a438d16d0f760f176
Purvi Poonia <waytopurvi2001@gmail.com>
2022-04-28 11:59:51 +0530

    create registration page

---
+++ swiftbuy_frontend/swiftbuy/src/app/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Registration_info } from '../registration_details';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  registerUserData:Registration_info = {
+    name: '',
+    email: '',
+    role: '',
+    address: '',
+    referral_token: '',
+    password: '',
+    phone: 0
+  };
+  constructor(private _auth : AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  registerUser(){
+    console.log(this.registerUserData);
+    this._auth.registerUser(this.registerUserData).subscribe(
+      response => {
+        console.log("registered new user. response = ",response)
+      },
+      error => {
+        console.log("eror in registering user = ",error);
+      }
+    );
+  }
+}
