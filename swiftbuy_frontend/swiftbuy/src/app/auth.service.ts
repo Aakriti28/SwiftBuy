012ea796405f50d6aff7d54815c99d8b57885c83
Gaurang Dev <gaurangdev@Gaurangs-MacBook-Air.local>
2022-04-29 10:50:07 +0530

    options updated

--- swiftbuy_frontend/swiftbuy/src/app/auth.service.ts
+++ swiftbuy_frontend/swiftbuy/src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Registration_info } from './registration_details';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +12,17 @@ export class AuthService {
   private _registerUrl = 'http://localhost:8000/signup';
   private _loginUrl = 'http://localhost:8000/login';
   private _logoutUrl = 'http://localhost:8000/logout';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,  private cookieService: CookieService) { }
 
   registerUser(new_user: Registration_info): Observable<any>{
-    return this.http.post(this._registerUrl, new_user);
+    return this.http.post(this._registerUrl, new_user, { headers: { 'Content-Type': 'application/json','X-CSRFToken': this.cookieService.get('csrftoken')  }, withCredentials: true });
   }
 
   loginUser(user: any): Observable<any>{
-    return this.http.post(this._loginUrl, user);
+    return this.http.post(this._loginUrl, user, { headers: { 'Content-Type': 'application/json','X-CSRFToken': this.cookieService.get('csrftoken')  }, withCredentials: true });
   }
 
   logoutUser(): Observable<any>{
-    return this.http.post(this._logoutUrl, {});
+    return this.http.post(this._logoutUrl, {}, { headers: { 'Content-Type': 'application/json','X-CSRFToken': this.cookieService.get('csrftoken')  }, withCredentials: true });
   }
 }
